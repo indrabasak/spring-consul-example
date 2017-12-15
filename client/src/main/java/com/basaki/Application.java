5d8c67665e747a1c937d00ef28820a9659a78140
Indra Basak <indra.basak1@gmail.com>
2017-12-15 10:48:42 -0800

    cleanup

--- client/src/main/java/com/basaki/Application.java
+++ client/src/main/java/com/basaki/Application.java
@@ -1,12 +1,10 @@
 package com.basaki;
 
-import com.basaki.config.CassandraProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
